ae45668e5c5dfb27b17f60363424a27fdf8f8410
Terrance Fisher <teefisher314@gmail.com>
2022-02-12 02:09:47 +0000

    finished the assignment

--- index.js
+++ index.js
@@ -15,13 +15,14 @@ let fuelTempCelsius = -225;
 let fuelLevel = 100;
 let weatherStatus = "clear";
 
-console.log("-------------------------------------\n" +"> LC04 - LAUNCH CHECKLIST\n" + "-------------------------------------\n" +"Date: " + date + "\nTime: " + time + "\n\n" + "-------------------------------------\n" + "> ASTRONAUT INFO\n" + "-------------------------------------\n" + "* count: " + astronautCount + "\n* status: " + astronautStatus + "\n\n-------------------------------------\n> FUEL DATA\n-------------------------------------");
+
+
 
 
 
 
 // Write code to generate the LC04 report here:
-
+console.log("-------------------------------------\n" +"> LC04 - LAUNCH CHECKLIST\n" + "-------------------------------------\n" +"Date: " + date + "\nTime: " + time + "\n\n" + "-------------------------------------\n" + "> ASTRONAUT INFO\n" + "-------------------------------------\n" + "* count: " + astronautCount + "\n* status: " + astronautStatus + "\n\n-------------------------------------\n> FUEL DATA\n-------------------------------------\n* Fuel temp celsius: " + fuelTempCelsius + " C\n* Fuel level: "+ fuelLevel +"%\n\n-------------------------------------\n> MASS DATA\n-------------------------------------\n* Crew mass: " + crewMassKg + " kg\n* Fuel mass: " + fuelMassKg + "kg\n* Shuttle mass: " + shuttleMassKg + " kg\n* Total mass: " + totalMassKg + " kg\n\n-------------------------------------\n> FLIGHT PLAN\n-------------------------------------\n* weather: " + weatherStatus + "\n\n-------------------------------------\n> OVERALL STATUS\n-------------------------------------\n* Clear for takeoff: Yes");
 
 
 
@@ -31,3 +32,8 @@ console.log("-------------------------------------\n" +"> LC04 - LAUNCH CHECKLIS
 
 
 // BONUS: Use readline-sync to prompt the user to enter the number of astronauts going on the mission.
+const input = require('readline-sync');
+
+let valueForAstronautCount = input.question("Enter the value for astronautCount ");
+console.log(crewMassKg);
+console.log(totalMassKg);
