82d995887647ca1675e7d47b677dfcd48b0d00b0
Terrance Fisher <teefisher314@gmail.com>
2022-02-12 01:33:59 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+// FORK this starter file and save it to your own Repl.it account.
+
+
+// Declare and initialize the 12 variables here:
+let date = "Monday 2019-3-18";
+let time = "10:05:34 AM";
+let astronautCount = 7;
+let astronautStatus = "ready";
+let averageAstronautMassKg = 80.7;
+let crewMassKg = astronautCount * averageAstronautMassKg;
+let fuelMassKg = 760000;
+let shuttleMassKg = 74842.31;
+let totalMassKg = crewMassKg + fuelMassKg + shuttleMassKg;
+let fuelTempCelsius = -225;
+let fuelLevel = 100;
+let weatherStatus = "clear";
+
+console.log("-------------------------------------\n" +"> LC04 - LAUNCH CHECKLIST\n" + "-------------------------------------\n" +"Date: " + date + "\nTime: " + time + "\n\n" + "-------------------------------------\n" + "> ASTRONAUT INFO\n" + "-------------------------------------\n" + "* count: " + astronautCount + "\n* status: " + astronautStatus + "\n\n-------------------------------------\n> FUEL DATA\n-------------------------------------");
+
+
+
+
+// Write code to generate the LC04 report here:
+
+
+
+
+// When done, have your TA check your code.
+
+
+
+
+// BONUS: Use readline-sync to prompt the user to enter the number of astronauts going on the mission.
